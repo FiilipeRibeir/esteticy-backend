a579d347e2dade73b71c8ee520b9c157d31fc2b6
FiilipeRibeir <filipecaldas3999@gmail.com>
2025-02-13 23:26:57 -0300

    fix: :white_check_mark: remove unnecessary Google logic and add user verification before creation

--- src/modules/auth/service/auth.service.ts
+++ src/modules/auth/service/auth.service.ts
@@ -1,13 +1,10 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { OAuth2Client } from 'google-auth-library';
 import { ClientLoginProps } from '../interface/auth_interface';
 
 @Injectable()
 export class AuthService {
-  private googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-
   constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
   async clientLogin(data: ClientLoginProps): Promise<{ access_token: string }> {
@@ -26,32 +23,5 @@ export class AuthService {
     return {
       access_token: this.jwtService.sign(payload),
     };
-  }
-
-  async googleLogin(idToken: string): Promise<{ access_token: string }> {
-    const ticket = await this.googleClient.verifyIdToken({
-      idToken,
-      audience: process.env.GOOGLE_CLIENT_ID,
-    });
-
-    const payload = ticket.getPayload();
-    if (!payload) {
-      throw new UnauthorizedException('Token inválido');
-    }
-
-    let user = await this.prisma.user.findUnique({ where: { email: payload.email } });
-
-    if (!user) {
-      user = await this.prisma.user.create({
-        data: {
-          email: payload.email || '',
-          name: payload.name || '',
-          nickname: payload.given_name || '',
-        },
-      });
-    }
-
-    const jwtPayload = { sub: user.id, email: user.email };
-    return { access_token: this.jwtService.sign(jwtPayload) };
   }
 }
